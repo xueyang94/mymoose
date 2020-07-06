8d52277d9323f6eef3839a689e55fd6e3bc870d6
Robert Carlsen <rwcarlsen@gmail.com>
2020-07-06 12:28:26 -0600

    add enthalpy line source/sink

---
+++ modules/porous_flow/src/dirackernels/PorousFlowEnthalpySink.C
@@ -0,0 +1,38 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PorousFlowEnthalpySink.h"
+#include "SinglePhaseFluidProperties.h"
+
+registerMooseObject("PorousFlowApp", PorousFlowEnthalpySink);
+
+InputParameters
+PorousFlowEnthalpySink::validParams()
+{
+  InputParameters params = PorousFlowPolyLineSink::validParams();
+  params.addRequiredParam<UserObjectName>("fp", "The name of the user object for fluid properties");
+  params.addRequiredCoupledVar("pressure", "Pressure");
+  params.addRequiredParam<Real>("T_in", "The incoming fluid temperature");
+  return params;
+}
+
+PorousFlowEnthalpySink::PorousFlowEnthalpySink(const InputParameters & parameters)
+  : PorousFlowPolyLineSink(parameters),
+    _pressure(coupledValue("pressure")),
+    _T_in(getPostprocessorValue("T_in")),
+    _fp(getUserObject<SinglePhaseFluidProperties>("fp"))
+{
+}
+
+Real
+PorousFlowEnthalpySink::computeQpBaseOutflow(unsigned current_dirac_ptid) const
+{
+  Real h = _fp.h_from_p_T(_pressure[_qp], _T_in);
+  return PorousFlowPolyLineSink::computeQpBaseOutflow(current_dirac_ptid) * h;
+}
