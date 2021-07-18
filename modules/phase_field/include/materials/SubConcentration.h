4bfdf5e4b9f61cf1c8e95a0358409c3efb4a46c3
xueyang94 <xueyangwuu@hotmail.com>
2021-07-18 15:18:15 -0400

    added extreme log Subconcentration and kks kernels

---
+++ modules/phase_field/include/materials/SubConcentration.h
@@ -0,0 +1,48 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Material.h"
+
+class SubConcentration : public Material
+{
+public:
+  static InputParameters validParams();
+
+  SubConcentration(const InputParameters & parameters);
+
+protected:
+  virtual void initQpStatefulProperties() override;
+  virtual void computeQpProperties() override;
+
+  const VariableValue & _c;
+  // MaterialProperty<Real> & _c_mat;
+  // const MaterialProperty<Real> & _c_mat_old;
+  const VariableValue & _eta;
+  // MaterialProperty<Real> & _eta_mat;
+  // const MaterialProperty<Real> & _eta_mat_old;
+  const MaterialProperty<Real> & _h;
+
+  MaterialProperty<Real> & _c1;
+  MaterialProperty<Real> & _c2;
+  const MaterialProperty<Real> & _c1_old;
+  const MaterialProperty<Real> & _c2_old;
+
+  const Real _abs_tol;
+
+  const Real _rel_tol;
+
+  const unsigned int _maxiter;
+
+  MaterialProperty<Real> & _dc1dc;
+  MaterialProperty<Real> & _dc1deta;
+  MaterialProperty<Real> & _dc2dc;
+  MaterialProperty<Real> & _dc2deta;
+};
