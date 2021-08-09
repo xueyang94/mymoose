67fdd923c2052fee776541459184baa8abefa772
xueyang94 <xueyangwuu@hotmail.com>
2021-08-09 14:07:43 -0400

    Parsed Material use

--- modules/phase_field/src/kernels/KKSACBulkF.C
+++ modules/phase_field/src/kernels/KKSACBulkF.C
@@ -14,25 +14,28 @@ registerMooseObject("PhaseFieldApp", KKSACBulkF);
 InputParameters
 KKSACBulkF::validParams()
 {
-  InputParameters params = Kernel::validParams();
+  // InputParameters params = Kernel::validParams();
+  InputParameters params = DerivativeMaterialInterface<Kernel>::validParams();
   params.addClassDescription("KKS model kernel (part 1 of 2) for the Bulk Allen-Cahn. This "
                              "includes all terms NOT dependent on chemical potential.");
   // params.addRequiredCoupledVar("global_c", "The interpolated concentration");
   // params.addRequiredCoupledVar("eta", "The order parameter");
-  params.addRequiredParam<MaterialPropertyName>("c1_name", "The name of c1");
-  params.addRequiredParam<MaterialPropertyName>("c2_name", "The name of c2");
+  // params.addRequiredParam<MaterialPropertyName>("c1_name", "The name of c1");
+  // params.addRequiredParam<MaterialPropertyName>("c2_name", "The name of c2");
   params.addRequiredParam<MaterialPropertyName>("dc1dc_name", "The name of dc1/dc");
   params.addRequiredParam<MaterialPropertyName>("dc2dc_name", "The name of dc2/dc");
   params.addRequiredParam<MaterialPropertyName>("dc1deta_name", "The name of dc1/deta");
   params.addRequiredParam<MaterialPropertyName>("dc2deta_name", "The name of dc2/deta");
-  params.addRequiredParam<MaterialPropertyName>("f1_name",
+  params.addRequiredParam<MaterialPropertyName>("F1_name",
                                                 "The name of the bulk energy of phase 1");
-  params.addRequiredParam<MaterialPropertyName>("f2_name",
+  params.addRequiredParam<MaterialPropertyName>("F2_name",
                                                 "The name of the bulk energy of phase 2");
-  params.addRequiredParam<MaterialPropertyName>("df1dc1_name",
-                                                "The name of the first derivative of f1 w.r.t. c1");
-  params.addRequiredParam<MaterialPropertyName>("df2dc2_name",
-                                                "The name of the first derivative of f2 w.r.t. c2");
+  // params.addRequiredParam<MaterialPropertyName>("df1dc1_name",
+  //                                               "The name of the first derivative of f1 w.r.t.
+  //                                               c1");
+  // params.addRequiredParam<MaterialPropertyName>("df2dc2_name",
+  //                                               "The name of the first derivative of f2 w.r.t.
+  //                                               c2");
   params.addRequiredParam<MaterialPropertyName>("L_name", "The name of the Allen-Cahn mobility");
   params.addRequiredParam<Real>("barrier_height", "Double well height parameter");
   params.addRequiredCoupledVar("w",
@@ -41,14 +44,14 @@ KKSACBulkF::validParams()
 }
 
 KKSACBulkF::KKSACBulkF(const InputParameters & parameters)
-  : Kernel(parameters),
-    // : DerivativeMaterialInterface<Kernel>(parameters),
+  // : Kernel(parameters),
+  : DerivativeMaterialInterface<Kernel>(parameters),
     // _c(coupledValue("global_c")),
     // _c_name(getVar("global_c", 0)->name()),
     // _eta(coupledValue("eta")),
     // _eta_name(getVar("eta", 0)->name()),
-    _c1(getMaterialProperty<Real>("c1_name")),
-    _c2(getMaterialProperty<Real>("c2_name")),
+    // _c1(getMaterialProperty<Real>("c1_name")),
+    // _c2(getMaterialProperty<Real>("c2_name")),
     // _dc1dc(getMaterialPropertyDerivative<Real>("c1_name", _c_name)),
     // _dc2dc(getMaterialPropertyDerivative<Real>("c2_name", _c_name)),
     // _dc1deta(getMaterialPropertyDerivative<Real>("c1_name", _eta_name)),
@@ -57,10 +60,14 @@ KKSACBulkF::KKSACBulkF(const InputParameters & parameters)
     _dc2dc(getMaterialProperty<Real>("dc2dc_name")),
     _dc1deta(getMaterialProperty<Real>("dc1deta_name")),
     _dc2deta(getMaterialProperty<Real>("dc2deta_name")),
-    _f1(getMaterialProperty<Real>("f1_name")),
-    _f2(getMaterialProperty<Real>("f2_name")),
-    _first_df1(getMaterialProperty<Real>("df1dc1_name")),
-    _first_df2(getMaterialProperty<Real>("df2dc2_name")),
+    _f1(getMaterialProperty<Real>("F1_name")),
+    _f2(getMaterialProperty<Real>("F2_name")),
+    // _first_df1(getMaterialProperty<Real>("df1dc1_name")),
+    // _first_df2(getMaterialProperty<Real>("df2dc2_name")),
+    _c1_name("c1"),
+    _c2_name("c2"),
+    _first_df1(getMaterialPropertyDerivative<Real>("F1_name", _c1_name)),
+    _first_df2(getMaterialPropertyDerivative<Real>("F2_name", _c2_name)),
     _L(getMaterialProperty<Real>("L_name")),
     _m(getParam<Real>("barrier_height")),
     _w_var(coupled("w"))
